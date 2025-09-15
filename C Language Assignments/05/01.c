0c47e79dcd45a404953deb1230038f26474a476c
AnshPatel191207 <patelansh191207@gmail.com>
2025-09-15 11:51:46 +0530

    c assignment 1 added

---
+++ C Language Assignments/05/01.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+    int n, i;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    for (i = 0; i < n; i++) printf("*");
+    return 0;
+}
---
+++ C Language Assignments/05/09.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int n, i, j;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    for (i = 1; i <= n; i++) {
+        for (j = 0; j < i; j++) printf("%c ", 'A' + j);
+        printf("\n");
+    }
+    return 0;
+}
